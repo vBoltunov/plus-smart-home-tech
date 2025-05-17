90b7554ed9e3c61cec79d1c0c2be6321673371ee
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-17 21:07:42 +0300

    Add CollectorController class

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/CollectorController.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.kafka.telemetry.collector.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.kafka.telemetry.collector.model.HubEvent;
+import ru.yandex.practicum.kafka.telemetry.collector.model.SensorEvent;
+import ru.yandex.practicum.kafka.telemetry.collector.service.CollectorService;
+
+@RestController
+@RequestMapping("/telemetry")
+@RequiredArgsConstructor
+@Slf4j
+public class CollectorController {
+    private final CollectorService collectorService;
+
+    @PostMapping("/sensors")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+        log.info("Received sensor event: {}", event);
+        collectorService.processSensorEvent(event);
+    }
+
+    @PostMapping("/hubs")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void collectHubEvent(@Valid @RequestBody HubEvent event) {
+        log.info("Received hub event: {}", event);
+        collectorService.processHubEvent(event);
+    }
+}
