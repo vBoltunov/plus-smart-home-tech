d5c3547e9da7b40c89ab3b6b53a94970be5fd1f9
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 12:27:41 +0300

    Change KafkaProperties to map hub-events-topic and sensor-events-topic

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
@@ -5,15 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 @Configuration
 @ConfigurationProperties(prefix = "spring.kafka")
 @Getter
 @Setter
 public class KafkaProperties {
     private String bootstrapServers;
-    private Map<String, String> topics;
+    private String hubEventsTopic;
+    private String sensorEventsTopic;
     private String keySerializerClass;
     private String valueSerializerClass;
 }
