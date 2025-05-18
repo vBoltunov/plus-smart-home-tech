0f22ce205830be534f18fe7166c6c77f5ee01daa
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 13:50:24 +0300

    Add javadoc to ClimateSensorEvent

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/model/ClimateSensorEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/model/ClimateSensorEvent.java
@@ -1,10 +1,23 @@
 package ru.yandex.practicum.kafka.telemetry.collector.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import ru.yandex.practicum.kafka.telemetry.collector.model.enums.SensorEventType;
 
+/**
+ * Represents a climate sensor event in the telemetry system.
+ *
+ * This class encapsulates climate-related sensor data, including temperature,
+ * humidity, and CO2 levels. It extends {@link SensorEvent} to inherit
+ * common event properties such as ID, hub ID, and timestamp.
+ *
+ * Fields:
+ * - `temperatureC` - Represents the temperature in Celsius.
+ * - `humidity` - Represents the relative humidity percentage.
+ * - `co2Level` - Represents the CO2 concentration in parts per million (ppm).
+ */
 @Getter
 @Setter
 @ToString(callSuper = true)
