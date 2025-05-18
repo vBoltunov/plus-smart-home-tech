a524cd4ffd19f0511ad59468c2155340cb2b9d11
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 11:00:06 +0300

    Change SensorEventMapper logic

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/mapper/SensorEventMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/mapper/SensorEventMapper.java
@@ -1,53 +1,85 @@
 package ru.yandex.practicum.kafka.telemetry.collector.mapper;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.kafka.telemetry.collector.model.ClimateSensorEvent;
-import ru.yandex.practicum.kafka.telemetry.collector.model.LightSensorEvent;
-import ru.yandex.practicum.kafka.telemetry.collector.model.MotionSensorEvent;
-import ru.yandex.practicum.kafka.telemetry.collector.model.SensorEvent;
-import ru.yandex.practicum.kafka.telemetry.collector.model.SwitchSensorEvent;
-import ru.yandex.practicum.kafka.telemetry.collector.model.TemperatureSensorEvent;
+import ru.yandex.practicum.kafka.telemetry.collector.model.*;
+import ru.yandex.practicum.kafka.telemetry.collector.model.enums.SensorEventType;
 import ru.yandex.practicum.kafka.telemetry.event.*;
 
 @Slf4j
 public class SensorEventMapper {
 
     public static SensorEventAvro toAvro(SensorEvent event) {
-        log.info("Mapping sensor event: {}, class: {}", event, event.getClass().getName());
+        log.info("Маппинг события датчика: {}, тип: {}, класс: {}", event, event.getType(), event.getClass().getSimpleName());
+
         SensorEventAvro.Builder builder = SensorEventAvro.newBuilder()
                 .setId(event.getId())
                 .setHubId(event.getHubId())
                 .setTimestamp(event.getTimestamp().toEpochMilli());
 
-        switch (event) {
-            case ClimateSensorEvent climate ->
-                    builder.setPayload(ClimateSensorAvro.newBuilder()
-                            .setTemperatureC(climate.getTemperatureC())
-                            .setHumidity(climate.getHumidity())
-                            .setCo2Level(climate.getCo2Level())
-                            .build());
-            case LightSensorEvent light ->
-                    builder.setPayload(LightSensorAvro.newBuilder()
-                            .setLinkQuality(light.getLinkQuality())
-                            .setLuminosity(light.getLuminosity())
-                            .build());
-            case MotionSensorEvent motion ->
-                    builder.setPayload(MotionSensorAvro.newBuilder()
-                            .setLinkQuality(motion.getLinkQuality())
-                            .setMotion(motion.getMotion())
-                            .setVoltage(motion.getVoltage())
-                            .build());
-            case SwitchSensorEvent sw ->
-                    builder.setPayload(SwitchSensorAvro.newBuilder()
-                            .setState(sw.getState())
-                            .build());
-            case TemperatureSensorEvent temp ->
-                    builder.setPayload(TemperatureSensorAvro.newBuilder()
-                            .setTemperatureC(temp.getTemperatureC())
-                            .setTemperatureF(temp.getTemperatureF())
-                            .build());
-            default ->
-                    throw new IllegalArgumentException("Unknown sensor event type: " + event.getClass().getSimpleName());
+        // Используем SensorEventType для определения типа события
+        SensorEventType eventType = event.getType();
+        log.debug("Тип события из getType(): {}", eventType);
+
+        switch (eventType) {
+            case CLIMATE_SENSOR_EVENT:
+                if (!(event instanceof ClimateSensorEvent climate)) {
+                    log.error("Несоответствие типа: ожидалось ClimateSensorEvent, получено {}", event.getClass().getSimpleName());
+                    throw new IllegalArgumentException("Ожидалось событие ClimateSensorEvent, но получено: " + event.getClass().getSimpleName());
+                }
+                builder.setPayload(ClimateSensorAvro.newBuilder()
+                        .setTemperatureC(climate.getTemperatureC())
+                        .setHumidity(climate.getHumidity())
+                        .setCo2Level(climate.getCo2Level())
+                        .build());
+                break;
+
+            case LIGHT_SENSOR_EVENT:
+                if (!(event instanceof LightSensorEvent light)) {
+                    log.error("Несоответствие типа: ожидалось LightSensorEvent, получено {}", event.getClass().getSimpleName());
+                    throw new IllegalArgumentException("Ожидалось событие LightSensorEvent, но получено: " + event.getClass().getSimpleName());
+                }
+                builder.setPayload(LightSensorAvro.newBuilder()
+                        .setLinkQuality(light.getLinkQuality())
+                        .setLuminosity(light.getLuminosity())
+                        .build());
+                break;
+
+            case MOTION_SENSOR_EVENT:
+                if (!(event instanceof MotionSensorEvent motion)) {
+                    log.error("Несоответствие типа: ожидалось MotionSensorEvent, получено {}", event.getClass().getSimpleName());
+                    throw new IllegalArgumentException("Ожидалось событие MotionSensorEvent, но получено: " + event.getClass().getSimpleName());
+                }
+                builder.setPayload(MotionSensorAvro.newBuilder()
+                        .setLinkQuality(motion.getLinkQuality())
+                        .setMotion(motion.getMotion())
+                        .setVoltage(motion.getVoltage())
+                        .build());
+                break;
+
+            case SWITCH_SENSOR_EVENT:
+                if (!(event instanceof SwitchSensorEvent sw)) {
+                    log.error("Несоответствие типа: ожидалось SwitchSensorEvent, получено {}", event.getClass().getSimpleName());
+                    throw new IllegalArgumentException("Ожидалось событие SwitchSensorEvent, но получено: " + event.getClass().getSimpleName());
+                }
+                builder.setPayload(SwitchSensorAvro.newBuilder()
+                        .setState(sw.getState())
+                        .build());
+                break;
+
+            case TEMPERATURE_SENSOR_EVENT:
+                if (!(event instanceof TemperatureSensorEvent temp)) {
+                    log.error("Несоответствие типа: ожидалось TemperatureSensorEvent, получено {}", event.getClass().getSimpleName());
+                    throw new IllegalArgumentException("Ожидалось событие TemperatureSensorEvent, но получено: " + event.getClass().getSimpleName());
+                }
+                builder.setPayload(TemperatureSensorAvro.newBuilder()
+                        .setTemperatureC(temp.getTemperatureC())
+                        .setTemperatureF(temp.getTemperatureF())
+                        .build());
+                break;
+
+            default:
+                log.error("Неизвестный тип события датчика: {}", eventType);
+                throw new IllegalArgumentException("Неизвестный тип события датчика: " + eventType);
         }
 
         return builder.build();
