996ec0d92a5dccd28627bcdda474676adfa4abbd
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 14:34:31 +0300

    AvroDeserializer: add more logging

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/serialization/AvroDeserializer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/serialization/AvroDeserializer.java
@@ -1,24 +1,21 @@
 package ru.yandex.practicum.kafka.telemetry.collector.serialization;
 
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.Decoder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.common.serialization.Deserializer;
 
-import java.util.Map;
+import java.io.IOException;
 
+@Slf4j
 public class AvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
     private final Class<T> type;
 
     public AvroDeserializer(Class<T> type) {
         this.type = type;
-    }
-
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {
-        // Конфигурация не требуется, тип передан через конструктор
     }
 
     @Override
@@ -27,16 +24,19 @@ public class AvroDeserializer<T extends SpecificRecordBase> implements Deseriali
             return null;
         }
         try {
-            DatumReader<T> datumReader = new SpecificDatumReader<>(type);
-            Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
-            return datumReader.read(null, decoder);
-        } catch (Exception e) {
+            BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(data, null);
+            SpecificDatumReader<T> reader = new SpecificDatumReader<>(type);
+            T result = reader.read(null, decoder);
+            log.info("Deserialized Avro data: type={}, topic={}, payload={}",
+                    type.getSimpleName(), topic,
+                    result instanceof ru.yandex.practicum.kafka.telemetry.event.HubEventAvro ?
+                            ((ru.yandex.practicum.kafka.telemetry.event.HubEventAvro) result).getPayload().getClass().getSimpleName() :
+                            result instanceof ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro ?
+                                    ((ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro) result).getPayload().getClass().getSimpleName() : "unknown");
+            return result;
+        } catch (IOException e) {
+            log.error("Failed to deserialize Avro data: type={}, topic={}", type.getSimpleName(), topic, e);
             throw new RuntimeException("Failed to deserialize Avro data", e);
         }
-    }
-
-    @Override
-    public void close() {
-        // Не требуется
     }
 }
