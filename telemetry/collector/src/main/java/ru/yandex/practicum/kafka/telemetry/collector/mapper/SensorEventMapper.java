0a74b839e22587a5ee630f39f3ec687091d7031f
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 14:05:25 +0300

    SensorEventMapper: change order in event selector

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/mapper/SensorEventMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/mapper/SensorEventMapper.java
@@ -17,29 +17,29 @@ public class SensorEventMapper {
 
         if (event instanceof ClimateSensorEvent climate) {
             builder.setPayload(ClimateSensorAvro.newBuilder()
-                    .setTemperatureC(climate.getTemperatureC())
-                    .setHumidity(climate.getHumidity())
-                    .setCo2Level(climate.getCo2Level())
+                    .setTemperatureC(climate.getTemperatureC() != null ? climate.getTemperatureC() : 0)
+                    .setHumidity(climate.getHumidity() != null ? climate.getHumidity() : 0)
+                    .setCo2Level(climate.getCo2Level() != null ? climate.getCo2Level() : 0)
                     .build());
         } else if (event instanceof LightSensorEvent light) {
             builder.setPayload(LightSensorAvro.newBuilder()
-                    .setLinkQuality(light.getLinkQuality())
-                    .setLuminosity(light.getLuminosity())
+                    .setLinkQuality(light.getLinkQuality() != null ? light.getLinkQuality() : 0)
+                    .setLuminosity(light.getLuminosity() != null ? light.getLuminosity() : 0)
                     .build());
         } else if (event instanceof MotionSensorEvent motion) {
             builder.setPayload(MotionSensorAvro.newBuilder()
-                    .setLinkQuality(motion.getLinkQuality())
-                    .setMotion(motion.getMotion())
-                    .setVoltage(motion.getVoltage())
+                    .setLinkQuality(motion.getLinkQuality() != null ? motion.getLinkQuality() : 0)
+                    .setMotion(motion.getMotion() != null ? motion.getMotion() : false)
+                    .setVoltage(motion.getVoltage() != null ? motion.getVoltage() : 0)
                     .build());
         } else if (event instanceof SwitchSensorEvent sw) {
             builder.setPayload(SwitchSensorAvro.newBuilder()
-                    .setState(sw.getState())
+                    .setState(sw.getState() != null ? sw.getState() : false)
                     .build());
         } else if (event instanceof TemperatureSensorEvent temp) {
             builder.setPayload(TemperatureSensorAvro.newBuilder()
-                    .setTemperatureC(temp.getTemperatureC())
-                    .setTemperatureF(temp.getTemperatureF())
+                    .setTemperatureC(temp.getTemperatureC() != null ? temp.getTemperatureC() : 0)
+                    .setTemperatureF(temp.getTemperatureF() != null ? temp.getTemperatureF() : 0)
                     .build());
         } else {
             log.error("Неподдерживаемый тип события датчика: {}", event.getClass().getSimpleName());
