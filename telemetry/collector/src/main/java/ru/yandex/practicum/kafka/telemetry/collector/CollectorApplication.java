d6c69a197b7e0b6535ae9e60134d03cc56f1cf6d
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 12:20:18 +0300

    Change CollectorApplication to include service bean names

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/CollectorApplication.java
@@ -2,10 +2,23 @@ package ru.yandex.practicum.kafka.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.yandex.practicum.kafka.telemetry.collector.service.CollectorService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
+    }
+
+    @Bean
+    public Map<String, CollectorService> eventServices(CollectorService collectorService) {
+        Map<String, CollectorService> services = new HashMap<>();
+        services.put("sensorEventService", collectorService);
+        services.put("hubEventService", collectorService);
+        return services;
     }
 }
