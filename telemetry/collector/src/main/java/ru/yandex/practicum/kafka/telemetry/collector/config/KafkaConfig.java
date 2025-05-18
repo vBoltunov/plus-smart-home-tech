36f1439508a82b4ed7ae346aabd70157074ea412
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-18 12:38:56 +0300

    Change KafkaConfig to use KafkaProperties for topic and server configuration

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaConfig.java
@@ -7,7 +7,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
@@ -17,30 +17,28 @@ import java.util.Properties;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    @Autowired
+    private KafkaProperties kafkaProperties;
 
     @Getter
-    @Value("${spring.kafka.hub-events-topic}")
     private String hubEventsTopic;
 
     @Getter
-    @Value("${spring.kafka.sensor-events-topic}")
     private String sensorEventsTopic;
 
+    @Getter
     private KafkaProducer<String, SpecificRecordBase> producer;
 
     @PostConstruct
     public void init() {
+        hubEventsTopic = kafkaProperties.getHubEventsTopic();
+        sensorEventsTopic = kafkaProperties.getSensorEventsTopic();
+
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ru.yandex.practicum.kafka.telemetry.collector.serialization.AvroSerializer.class.getName());
 
         producer = new KafkaProducer<>(props);
-    }
-
-    public KafkaProducer<String, SpecificRecordBase> getProducer() {
-        return producer;
     }
 }
