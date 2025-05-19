e38d49cc10fd9a995f77e22f8ae119c88d1a76c0
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-19 14:54:47 +0300

    ClimateSensorEvent: add necessary annotations

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/model/ClimateSensorEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/model/ClimateSensorEvent.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.kafka.telemetry.collector.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.FieldDefaults;
 import ru.yandex.practicum.kafka.telemetry.collector.model.enums.SensorEventType;
 
 /**
@@ -20,10 +22,14 @@ import ru.yandex.practicum.kafka.telemetry.collector.model.enums.SensorEventType
 @Getter
 @Setter
 @ToString(callSuper = true)
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class ClimateSensorEvent extends SensorEvent {
-    private Integer temperatureC;
-    private Integer humidity;
-    private Integer co2Level;
+    @NotNull
+    Integer temperatureC;
+    @NotNull
+    Integer humidity;
+    @NotNull
+    Integer co2Level;
 
     @Override
     public SensorEventType getType() {
