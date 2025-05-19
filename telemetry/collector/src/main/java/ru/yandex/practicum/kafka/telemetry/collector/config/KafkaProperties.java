3e9175edc4da70772441f26229e1d32941300ff0
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-19 18:52:42 +0300

    KafkaProperties: use @FieldDefaults instead of multiple private fields

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.kafka.telemetry.collector.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +12,10 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "kafka")
 @Getter
 @Setter
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class KafkaProperties {
-    private String bootstrapServer;
-    private Map<String, String> topics;
-    private String keySerializeClass;
-    private String valueSerializeClass;
+    String bootstrapServer;
+    Map<String, String> topics;
+    String keySerializeClass;
+    String valueSerializeClass;
 }
