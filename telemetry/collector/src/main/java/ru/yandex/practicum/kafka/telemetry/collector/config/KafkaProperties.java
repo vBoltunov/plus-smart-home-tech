5254dcc5bd15ad50ce578ba9bc065c38cfcb9854
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-19 14:03:06 +0300

    KafkaProperties: change logic

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/config/KafkaProperties.java
@@ -5,14 +5,15 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
-@ConfigurationProperties(prefix = "spring.kafka")
+@ConfigurationProperties(prefix = "kafka")
 @Getter
 @Setter
 public class KafkaProperties {
-    private String bootstrapServers;
-    private String hubEventsTopic;
-    private String sensorEventsTopic;
-    private String keySerializerClass;
-    private String valueSerializerClass;
+    private String bootstrapServer;
+    private Map<String, String> topics;
+    private String keySerializeClass;
+    private String valueSerializeClass;
 }
