74761c5c77a85ff5a84e946bbc6d0de0413341d7
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-28 19:55:36 +0300

    Add KafkaConfig

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/config/KafkaConfig.java
@@ -0,0 +1,57 @@
+package ru.yandex.practicum.kafka.telemetry.aggregator.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import ru.yandex.practicum.kafka.telemetry.aggregator.serialization.SensorEventDeserializer;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, SensorEventAvro> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaConsumer<String, SensorEventAvro> kafkaConsumer(ConsumerFactory<String, SensorEventAvro> consumerFactory) {
+        return new KafkaConsumer<>(consumerFactory.getConfigurationProperties());
+    }
+
+    @Bean
+    public ProducerFactory<String, SensorsSnapshotAvro> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer(ProducerFactory<String, SensorsSnapshotAvro> producerFactory) {
+        return new KafkaProducer<>(producerFactory.getConfigurationProperties());
+    }
+}
