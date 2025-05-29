0ae695b70d1c9e5c56fec9bec552a5100a46c7c5
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-29 11:02:27 +0300

    Aggregator: add exclusions

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
@@ -2,8 +2,9 @@ package ru.yandex.practicum.kafka.telemetry.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class Aggregator {
     public static void main(String[] args) {
         SpringApplication.run(Aggregator.class, args);
