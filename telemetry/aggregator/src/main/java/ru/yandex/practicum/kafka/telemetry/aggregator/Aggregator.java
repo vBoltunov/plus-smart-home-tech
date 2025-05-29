2b525c55f395c66a61566803893c6f56a03b6585
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-29 10:42:52 +0300

    Aggregator: change logic

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
@@ -2,15 +2,10 @@ package ru.yandex.practicum.kafka.telemetry.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class Aggregator {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
-        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
-        aggregator.start();
+        SpringApplication.run(Aggregator.class, args);
     }
 }
