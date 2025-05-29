69a8de7887489533c997f5e603e2855b64960870
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-29 11:38:06 +0300

    SnapshotService: change logic

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/service/SnapshotService.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/service/SnapshotService.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.kafka.telemetry.aggregator.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
-import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.yandex.practicum.kafka.telemetry.event.*;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -22,36 +20,83 @@ public class SnapshotService {
         String hubId = event.getHubId();
         String sensorId = event.getId();
         long eventTimestampMillis = event.getTimestamp();
-        Instant eventTimestamp = Instant.ofEpochMilli(eventTimestampMillis);
 
-        // Получаем или создаём снапшот для хаба
-        SensorsSnapshotAvro snapshot = snapshots.computeIfAbsent(hubId, k -> {
-            SensorsSnapshotAvro newSnapshot = new SensorsSnapshotAvro();
-            newSnapshot.setHubId(hubId);
-            newSnapshot.setTimestamp(eventTimestamp);
-            newSnapshot.setSensorsState(new HashMap<>());
-            return newSnapshot;
+        log.debug("Обновление снапшота: hubId={}, sensorId={}, timestamp={}", hubId, sensorId, eventTimestampMillis);
+
+        SensorsSnapshotAvro snapshot = snapshots.computeIfAbsent(hubId, id -> {
+            log.info("Создан новый снапшот для хаба: {}", hubId);
+            SensorsSnapshotAvro snap = new SensorsSnapshotAvro();
+            snap.setHubId(hubId);
+            snap.setTimestamp(Instant.ofEpochMilli(eventTimestampMillis));
+            snap.setSensorsState(new HashMap<>());
+            return snap;
         });
 
-        // Проверяем текущее состояние датчика
-        SensorStateAvro oldState = snapshot.getSensorsState().get(sensorId);
-        if (oldState != null && (oldState.getTimestamp().toEpochMilli() >= eventTimestampMillis ||
-                oldState.getData().equals(event.getPayload()))) {
-            log.debug("No update needed for sensor {} in hub {}", sensorId, hubId);
-            return Optional.empty();
+        Map<String, SensorStateAvro> sensorsState = snapshot.getSensorsState();
+        SensorStateAvro oldState = sensorsState.get(sensorId);
+
+        if (oldState != null) {
+            long oldTs = oldState.getTimestamp().toEpochMilli();
+            if (oldTs >= eventTimestampMillis) {
+                log.debug("Пропущено устаревшее событие: старый ts={}, новый ts={}", oldTs, eventTimestampMillis);
+                return Optional.empty();
+            }
+
+            Object newPayload = event.getPayload();
+            Object oldPayload = oldState.getData();
+
+            if (!oldPayload.getClass().equals(newPayload.getClass())) {
+                log.warn("Типы payload не совпадают: old={}, new={}", oldPayload.getClass(), newPayload.getClass());
+            } else if (newPayload instanceof ClimateSensorAvro newClimate &&
+                    oldPayload instanceof ClimateSensorAvro oldClimate) {
+                if (oldClimate.getTemperatureC() == newClimate.getTemperatureC() &&
+                        oldClimate.getHumidity() == newClimate.getHumidity() &&
+                        oldClimate.getCo2Level() == newClimate.getCo2Level()) {
+                    log.debug("ClimateSensorAvro: данные не изменились");
+                    return Optional.empty();
+                }
+            } else if (newPayload instanceof LightSensorAvro newLight &&
+                    oldPayload instanceof LightSensorAvro oldLight) {
+                if (oldLight.getLinkQuality() == newLight.getLinkQuality() &&
+                        oldLight.getLuminosity() == newLight.getLuminosity()) {
+                    log.debug("LightSensorAvro: данные не изменились");
+                    return Optional.empty();
+                }
+            } else if (newPayload instanceof MotionSensorAvro newMotion &&
+                    oldPayload instanceof MotionSensorAvro oldMotion) {
+                if (oldMotion.getLinkQuality() == newMotion.getLinkQuality() &&
+                        oldMotion.getMotion() == newMotion.getMotion() &&
+                        oldMotion.getVoltage() == newMotion.getVoltage()) {
+                    log.debug("MotionSensorAvro: данные не изменились");
+                    return Optional.empty();
+                }
+            } else if (newPayload instanceof SwitchSensorAvro newSwitch &&
+                    oldPayload instanceof SwitchSensorAvro oldSwitch) {
+                if (oldSwitch.getState() == newSwitch.getState()) {
+                    log.debug("SwitchSensorAvro: данные не изменились");
+                    return Optional.empty();
+                }
+            } else if (newPayload instanceof TemperatureSensorAvro newTemp &&
+                    oldPayload instanceof TemperatureSensorAvro oldTemp) {
+                if (oldTemp.getTemperatureC() == newTemp.getTemperatureC() &&
+                        oldTemp.getTemperatureF() == newTemp.getTemperatureF()) {
+                    log.debug("TemperatureSensorAvro: данные не изменились");
+                    return Optional.empty();
+                }
+            } else {
+                log.warn("Необрабатываемый тип сенсора: {}", newPayload.getClass().getSimpleName());
+            }
+            log.debug("Обновление состояния датчика {}: старое ts={}, новое ts={}", sensorId, oldTs, eventTimestampMillis);
+        } else {
+            log.debug("Добавление нового датчика в снапшот: {}", sensorId);
         }
 
-        // Создаём новое состояние датчика
         SensorStateAvro newState = new SensorStateAvro();
-        newState.setTimestamp(eventTimestamp);
+        newState.setTimestamp(Instant.ofEpochMilli(eventTimestampMillis));
         newState.setData(event.getPayload());
-
-        // Обновляем снапшот
-        snapshot.getSensorsState().put(sensorId, newState);
-        snapshot.setTimestamp(eventTimestamp);
-        snapshots.put(hubId, snapshot);
-
-        log.info("Updated snapshot for hub {} with sensor {}", hubId, sensorId);
+        sensorsState.put(sensorId, newState);
+        snapshot.setTimestamp(Instant.ofEpochMilli(eventTimestampMillis));
+        log.info("Снапшот обновлён для хаба {}: sensorId={}, timestamp={}", hubId, sensorId, eventTimestampMillis);
         return Optional.of(snapshot);
     }
 }
