0f3e784da5c2f054080c08dfb4bf984cf7a7cc25
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-31 15:43:20 +0300

    Add Scenario entity

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/model/Scenario.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.telemetry.analyzer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "scenarios", uniqueConstraints = @UniqueConstraint(columnNames = {"hub_id", "name"}))
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Scenario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hub_id", nullable = false)
+    private String hubId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
