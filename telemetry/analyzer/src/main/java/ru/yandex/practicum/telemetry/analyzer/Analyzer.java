c6845d0ebe3edff39dfb3a9b6aa4df6cdd38c038
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-05-31 23:40:28 +0300

    Analyzer.java: change logic

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
@@ -1,3 +1,4 @@
+// ru.yandex.practicum.telemetry.analyzer.Analyzer.java
 package ru.yandex.practicum.telemetry.analyzer;
 
 import net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration;
@@ -6,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.telemetry.analyzer.processor.HubEventProcessor;
-import ru.yandex.practicum.telemetry.analyzer.processor.SnapshotProcessor;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -15,16 +14,6 @@ import ru.yandex.practicum.telemetry.analyzer.processor.SnapshotProcessor;
 public class Analyzer {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Analyzer.class, args);
-
-        HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
-        SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
-
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-
-        snapshotProcessor.start();
-
         Runtime.getRuntime().addShutdownHook(new Thread(context::close));
     }
 }
