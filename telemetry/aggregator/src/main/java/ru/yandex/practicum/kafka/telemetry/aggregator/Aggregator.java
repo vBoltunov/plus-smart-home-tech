5ac5578b734307746c0c52dad49087987790ab62
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-06-07 14:52:01 +0300

    aggregator/Aggregator: add annotations

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/Aggregator.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.kafka.telemetry.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@ConfigurationPropertiesScan
 public class Aggregator {
     public static void main(String[] args) {
         SpringApplication.run(Aggregator.class, args);
