0a4b6d89cf79fe345029aa0881dba0c5ee022105
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-06-15 20:33:22 +0300

    interaction-api: add enums

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/interaction_api/enums/Category.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.commerce.interaction_api.enums;
+
+public enum Category {
+    CONTROL, SENSORS, LIGHTING
+}
---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/interaction_api/enums/Status.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.commerce.interaction_api.enums;
+
+public enum Status {
+    ACTIVE, DEACTIVATE
+}
