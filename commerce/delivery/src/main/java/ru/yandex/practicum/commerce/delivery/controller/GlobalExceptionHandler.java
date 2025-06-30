425c0e47388ddbcc3b46a8a26856e5537d90202d
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-06-30 20:40:07 +0300

    delivery: add GlobalExceptionHandler

---
+++ commerce/delivery/src/main/java/ru/yandex/practicum/commerce/delivery/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.commerce.delivery.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.commerce.interaction_api.exceptions.NoDeliveryFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoDeliveryFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> handleNoDeliveryFoundException(NoDeliveryFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    private Map<String, Object> buildErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("httpStatus", status.toString());
+        response.put("userMessage", message);
+        response.put("message", message);
+        return response;
+    }
+}
